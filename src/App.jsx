ff1c716ff4cb583241bde3a29a5a1f44342aeedd
raytaliman <raytaliman@icloud.com>
2024-04-16 16:06:18 +0800

    navbar added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
 import Userlogo from './assets/Userlogo.jsx';
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/react";
-import Homepage from './pages/Homepage.jsx';
-import {Button, ButtonGroup} from "@nextui-org/react";
+import {Link, Input} from "@nextui-org/react";
+
+
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Navbar>
+   <Navbar isBordered>
       <NavbarBrand>
         <p className="font-bold text-inherit">ACME</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Route  element = {<Homepage />} />
-        </NavbarItem>
-        <NavbarItem>
-          <Button color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
     </Navbar>
-    </Routes>
-  </BrowserRouter>
-
+    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <Input type="email" label="Email" />
+    </div>
     </>
   )
 }
