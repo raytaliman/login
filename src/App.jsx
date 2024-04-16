537e2021c1e0b86a685d4da150ab0377119d0d8e
raytaliman <raytaliman@icloud.com>
2024-04-16 16:38:00 +0800

    separated navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
-import Userlogo from './assets/Userlogo.jsx';
 import './App.css'
-import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/react";
 import {Link, Input} from "@nextui-org/react";
+import Nav from "./components/Nav.jsx";
+
+
+
 
 
 const App = () => {
   return (
     <>
-   <Navbar isBordered>
-      <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-    </Navbar>
-    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Input type="email" label="Email" />
+    <Nav />
+    <div className="container mx-auto object-none object-center">
+      <div className="flex flex-col m-8">
+        <div className="grid grid-cols-3 gap-4">
+          <div></div>
+          <div><div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input type="email" label="Email" />
+        </div></div>
+          <div></div>
+        </div>
+      </div>
+     
     </div>
+
+    
+
     </>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/react";
+const Nav = () => {
+  return (
+        <Navbar isBordered>
+                <NavbarBrand>
+                        <p className="font-bold text-inherit">RT</p>
+                </NavbarBrand>
+         </Navbar>
+  )
+}
+
+export default Nav
